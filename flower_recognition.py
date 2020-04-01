abd2d54b13b28c464891d0ce518626b049832348
Saakshi Shah <54411640+SaakshiShah25@users.noreply.github.com>
2020-04-01 12:21:16 +0530

    Update flower_recognition.py

--- flower_recognition.py
+++ flower_recognition.py
@@ -18,17 +18,17 @@ from keras.layers import BatchNormalization
 classifier=Sequential()
 
 #Step 1 Convolution
-classifier.add(Convolution2D(32,(3,3),input_shape=(128,128,3),activation='relu'))
+classifier.add(Convolution2D(32,(3,3),input_shape=(64,64,3),activation='relu'))
 #Step 2 Pooling
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 #Adding second convolution layer
-classifier.add(Convolution2D(64,(3,3),activation='relu'))
+classifier.add(Convolution2D(32,(3,3),activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
-#Adding third convolution layer
-classifier.add(Convolution2D(64,(3,3),activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2,2)))
+'''#Adding third convolution layer
+classifier.add(Convolution2D(32,(3,3),activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))'''
 
 #Step 3 Flattening
 classifier.add(Flatten())
@@ -36,19 +36,19 @@ classifier.add(Flatten())
 #Step 4 Fully Connected layer
 
 #for the hidden layer
-classifier.add(Dense(activation='relu',units=512))
+classifier.add(Dense(activation='relu',units=128,init='uniform'))
 classifier.add(Dropout(0.3))
 classifier.add(BatchNormalization(axis=1))
 #for the second hidden layer
-classifier.add(Dense(activation='relu',units=512))
+classifier.add(Dense(activation='relu',units=128))
 classifier.add(Dropout(0.3))
 classifier.add(BatchNormalization(axis=1))
 #for the third hidden layer
-classifier.add(Dense(activation='relu',units=512))
+'''classifier.add(Dense(activation='relu',units=128))
 classifier.add(Dropout(0.3))
-classifier.add(BatchNormalization(axis=1))
+classifier.add(BatchNormalization(axis=1))'''
 #for the output layer
-classifier.add(Dense(activation='softmax',units=5))
+classifier.add(Dense(activation='softmax',units=5,init='uniform'))
 
 #Compiling the CNN
 classifier.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
@@ -68,20 +68,20 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 #Applying augmentation to the training set
 training_set = train_datagen.flow_from_directory(
         'flowers/training_set',
-        target_size=(128,128),
-        batch_size=32,
+        target_size=(64,64),
+        batch_size=10,
         class_mode='categorical')
 
 #Applying augmentation to the test set
 test_set = test_datagen.flow_from_directory(
         'flowers/test_set',
-        target_size=(128,128),
-        batch_size=32,
+        target_size=(64,64),
+        batch_size=10,
         class_mode='categorical')
 
 classifier.fit_generator(
         training_set,
         steps_per_epoch=3463,
-        epochs=25,
+        epochs=50,
         validation_data=test_set,
         validation_steps=860)
